cac4a96aa8ff960b130e39f7e1555e20d5ab80c3
AMEENTA014 <ameenta41@gmail.com>
2025-12-26 21:10:38 +0530

    cant run in arch since there is no window manager

--- game.cpp
+++ game.cpp
@@ -1,12 +1,43 @@
 #include<iostream>// use the in and out files so we  can see things in terminal 
 #include<SDL3/SDL.h>//inorder to use the abstraction for using the winman loops and render 
 				     // also for using sdl3 we need this file
-int main(){
-SDL_Init(SDL_INIT_VIDEO);
-SDL_WINDOW *window = SDL_CreateWindow(
-		"sdl3game " , 
-		800, 600 
+int main(int  , char ** ){
+if(SDL_Init(SDL_INIT_VIDEO)!=0){std::cerr << "dl_init failed" << SDL_GetError() << "\n" ; return 1;}//initialise the window so that some systems and default configs are set underneath
+SDL_Window *window = SDL_CreateWindow(//creating a window by requesting the window manager 
+		"sdl3game " , //title of the game and this structure is used for managing the windows
+		800, 600, 
+	       0	//this flag says the window can be resizable 
 		
-		)
+		); 
+SDL_Renderer * renderer = SDL_CreateRenderer(window,nullptr); 
+bool running = true;
+float x = 0.0f; 
+while(running){
+SDL_Event event; 
+   while(SDL_PollEvent(&event)){
+        if(event.type == SDL_EVENT_QUIT){
+	  running =  false; 
+	}
+ 
+     }
+   x+=2.0f; 
+   if(x>800)x=0; 
+   SDL_SetRenderDrawColor(renderer,0,0,0,255); 
+   SDL_RenderClear(renderer); 
+   SDL_SetRenderDrawColor(renderer,255,0,0,255);
+   SDL_FRect rect = {x, 200, 50 , 50 }; 
+   SDL_RenderFillRect(renderer, &rect); 
+   SDL_RenderPresent(renderer);
+   SDL_Delay(16);
 
+    }
+	//loop and in the loop clear screen with color 
+	//draw the square in some position 
+	//change the var for different position 
+	//render it 
+	//loop again
+SDL_DestroyRenderer(renderer); 
+SDL_DestroyWindow(window); 
+SDL_Quit(); 
+return 0 ; 
 }
---
+++ new.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main(int, char**)
+{
+    std::cout << "Starting program...\n";
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr <<" SDL_Init failed:"  << SDL_GetError() << "\n";
+        return 1;
+    }
+    std::cout << "SDL_Init OK\n";
+
+    SDL_Window* window = SDL_CreateWindow("SDL debug", 800, 600, 0);
+if(!window){
+	std::cerr <<" CreateWindow failed:"  << SDL_GetError() << "\n";
+        return 1;
+    }
+    std::cout << "Window OK\n";
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+if(!renderer){ 
+ 	std::cerr <<" CreateRenderer failed:"  << SDL_GetError() << "\n";
+        return 1;
+    }
+    std::cout << "Renderer OK\n";
+
+    bool running = true;
+    SDL_Event e;
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    std::cout << "Exiting cleanly...n";
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0 ; 
+}
