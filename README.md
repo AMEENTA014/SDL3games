22d530b4ae66bc0fc2f779111a7533bc9401d4dd
AMEENTA014 <ameenta41@gmail.com>
2025-12-27 11:25:03 +0530

    now any smart people can understand the code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SDL3games
-prerequisites 
+<<prerequisites and developer installations >> 
 --g++ binary 
 --SDL3 library 
 
@@ -7,6 +7,11 @@ if its windows
    install mingw and install the sdl3 package manually 
    or use the msys2 pacman to install the sdl3 
    then link the cpp file to the SDL dll and execute 
+   notpad or cursor as IDE 
+
+
+
+
 
 if its arch or debian in wsl2 
    enable wsl2 
@@ -15,10 +20,22 @@ if its arch or debian in wsl2
    install sdl3 
    compile and link 
    execute 
+   vim / nvim or emacs or notepad.exe 
+
+
+
 
 if its linux 
    just install the sdl3 package 
    execute the code by linking 
+   vim nvim or emacs 
+
+
 
 if its mac idk will update it later 
    but i assume its same like linux compilation 
+
+<<how to run this game >>
+clone the repo and add the path to env 
+then use the executable suitable for your os 
+game.exe for win and game for linux 
--- game.cpp
+++ game.cpp
@@ -1,48 +1,41 @@
-#include<iostream>// use the in and out files so we  can see things in terminal 
-#include<SDL3/SDL.h>//inorder to use the abstraction for using the winman loops and render 
-				     // also for using sdl3 we need this file
+#include<iostream>//using this lib , the functions provide abstraction to handle text input and output 
+#include<SDL3/SDL.h>//this lib provides the method to create window , renderer and other display functions 
 int main(int  , char ** ){
-if(SDL_Init(SDL_INIT_VIDEO)==false){
-  std::cerr << "this is error ofc"<<SDL_GetError() << "\n" ; 
+if(SDL_Init(SDL_INIT_VIDEO)==false){//init intitializes subsystems , first we need video for window creation 
+  std::cerr << "this is error ofc"<<SDL_GetError() << "\n" ;//returns false means error in initialization 
   return 1 ; 
 };
-std::cout << "this is fine "; //initialise the window so that some systems and default configs are set underneath
 SDL_Window *window = SDL_CreateWindow(//creating a window by requesting the window manager 
-		"sdl3game " , //title of the game and this structure is used for managing the windows
+		"pacman " , //title of the game and this structure is used for managing the windows
 		800, 600, 
 	       0	//this flag says the window can be resizable 
 		
 		); 
-SDL_Renderer * renderer = SDL_CreateRenderer(window,nullptr); 
-bool running = true;
+SDL_Renderer * renderer = SDL_CreateRenderer(window,nullptr);//inorder to draw to the window we need renderer  
+bool running = true;//running should be true means currently the window is working 
 float x = 0.0f; 
-float y = 600.0f; 
+float y = 600.0f; //coordinates for the box to appear 
 while(running){
-SDL_Event event; 
-   while(SDL_PollEvent(&event)){
-        if(event.type == SDL_EVENT_QUIT){
-	  running =  false; 
+SDL_Event event;//structure for the event , which is used to store the events from the os  
+   while(SDL_PollEvent(&event)){//move the events to the event structure for processing 
+        if(event.type == SDL_EVENT_QUIT){//checking the user did press the close button 
+	  running =  false; //not exiting from the process but exit  the loop
 	}
  
      }
    x+=2.0f; 
    y-=2.0f;
    if(x>800){x=0;y=600;  }
-   SDL_SetRenderDrawColor(renderer,0,0,0,255); 
+   SDL_SetRenderDrawColor(renderer,0,0,0,255); //setting the color and clearing the back buffer 
    SDL_RenderClear(renderer); 
-   SDL_SetRenderDrawColor(renderer,255,0,0,255);
+   SDL_SetRenderDrawColor(renderer,255,0,0,255);//set color again and create a rectangle structure 
    SDL_FRect rect = {x, y, 50 , 50 }; 
-   SDL_RenderFillRect(renderer, &rect); 
+   SDL_RenderFillRect(renderer, &rect);//draw the rectangle using the renderer 
    SDL_RenderPresent(renderer);
    SDL_Delay(16);
 
     }
-	//loop and in the loop clear screen with color 
-	//draw the square in some position 
-	//change the var for different position 
-	//render it 
-	//loop again
-SDL_DestroyRenderer(renderer); 
+SDL_DestroyRenderer(renderer);//after that destory the renderer and th window  then quit the initialization 
 SDL_DestroyWindow(window); 
 SDL_Quit(); 
 return 0 ; 
